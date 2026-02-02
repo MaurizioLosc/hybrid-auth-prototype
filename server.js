35723d4f6a7c841bbcfa7c2358627dfd8932b9ed
MaurizioLosc <ddhlytked5@gmx.us>
2026-02-03 04:53:08 +0530

    Update

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
+require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const ConnectDB = require('./config/db');
+
 
 const app = express();
 
 // CONFIGS
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET = process.env.JWT_SECRET || 'SECRET_KEY';
+
+// database connection
+ConnectDB()
+
+// Globle Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.set('jwt-secret', JWT_SECRET);
+
+// health check
+app.get('/health', (req, res) => {
+    res.json({
+        status: 'OK',
+        service: 'Hybrid Authorization Prototype',
+        timestamp: new Date()
+    });
+});
+
+// globle error healder
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        error: 'Internal Server Error',
+        message: err.message
+    });
+});
+
+// server start
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
