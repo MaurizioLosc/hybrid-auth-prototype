c1fbe21d0fc8e440a431e93acf31120307ba9575
MaurizioLosc <ddhlytked5@gmx.us>
2026-02-03 07:13:27 +0530

    Update authorize.js

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,13 +1,24 @@
 const policies = require('../policies/policies.json');
 const Override = require('../models/override.model');
 
-module.exports = async(req, res, next) => {
+module.exports = async (req, res, next) => {
     const { role } = req.user
     const action = req.action
 
     // get based policy
     const basePolicy = policies[role]?.includes(action)
 
-    
+    // find any policy overrides
+    const override = await Override.findOne({
+        adminId: req.user.id,
+        action,
+        expiresAt: { $gt: new Date() }
+    });
+
+    if (!basePolicy && !override)
+        return res.status(403).json({ error: 'Access denied' });
+
+    req.overrideUsed = !!override;
+    next();
 }
 
